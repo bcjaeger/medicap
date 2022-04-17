1a0724074999c8fad04d3def542ead7dde43d253
bcjaeger <byron.jaeger@gmail.com>
2022-04-17 13:27:43 -0400

    don't need dashboard loading

--- packages.R
+++ packages.R
@@ -1,11 +1,10 @@
 
 
 library(shiny)
-library(shinydashboard)
-library(shinydashboardPlus)
 library(tidyverse)
 library(shinyWidgets)
 library(data.table)
 library(gt)
 library(glue)
 library(table.glue)
+library(rintrojs)
